bf3e0181811805fee5aefb83d3ba05cd4501ebbb
joemecha <jwmecha@gmail.com>
2021-03-04 18:45:55 -0600

    Create night_writer_test

---
+++ lib/night_writer.rb
@@ -0,0 +1,5 @@
+class NightWriter
+
+
+puts "Created 'braille.txt' containing 4 characters"
+end
---
+++ test/night_writer_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/night_writer'
+
+class NightWriterTest < Minitest::Test
+  def setup
+    @night_writer = NightWriter.new
+  end
+
+  def test_it_exists
+    assert_instance_of NightWriter, @night_writer
+  end
+
+  def test_it_has_attributes
+    skip
+    assert_equal
+  end
+
+  def test_prints_message_to_terminal
+    skip 
+    assert_output(stdout = /Created 'braille.txt' containing 4 characters/,
+                  stderr = nil)
+  end
+end
