64b901d8e0be9f1d03c55b11de4abbc6fae85ced
joemecha <jwmecha@gmail.com>
2021-03-04 21:33:06 -0600

    Attempt test_print_message_to_user

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require './lib/night_writer'
 
 class NightWriterTest < Minitest::Test
@@ -11,9 +12,12 @@ class NightWriterTest < Minitest::Test
     assert_instance_of NightWriter, @night_writer
   end
 
-  def test_prints_message_to_terminal
-    skip
-    assert_output(stdout = /Created 'braille.txt' containing 4 characters/,
-                  stderr = nil)
+  def test_prints_message_to_user
+    @write_file_name = "braille.txt"
+    @read_file_characters = 26
+
+    expected = "Created 'braille.txt' containing 26 characters"
+
+    assert_equal expected, @night_writer.message_to_user
   end
 end
