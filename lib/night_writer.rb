669b85d19947739784a194ed2286c773b1dfe27d
joemecha <jwmecha@gmail.com>
2021-03-05 11:17:26 -0600

    Complete iteration 1 - input read write message-to-user

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,26 +1,36 @@
 class NightWriter
-  # attr_reader :user_input
 
   def initialize
-    @user_input = gets.chomp.split
+    @user_input = get_input
     @read_file_name = @user_input[1]
     @write_file_name = @user_input[2]
-    @read_file_characters = 0
+    @read_file_chars = 0
   end
 
   def compose
-    # refactor into various methods, classes, etc
+    read_message_file
+    write_file
+    message_to_user
+  end
+
+  def read_message_file
     read_file = File.readlines("#{@read_file_name}").each do |line|
-      @read_file_characters += line.chomp.length
+      @read_file_chars += line.chomp.length
       puts line
     end
+  end
 
-
+  def write_file
     write_file = File.open("#{@write_file_name}", 'w')
     write_file.puts "NightWriter is still in progress"
     write_file.close
-
-    p "Created '#{@write_file_name}' containing #{@read_file_characters} characters"
   end
 
+  def get_input
+    gets.chomp.split
+  end
+
+  def message_to_user
+    p "Created '#{@write_file_name}' containing #{@read_file_chars} characters"
+  end
 end
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -11,13 +11,4 @@ class NightWriterTest < Minitest::Test
   def test_it_exists
     assert_instance_of NightWriter, @night_writer
   end
-
-  def test_prints_message_to_user
-    @write_file_name = "braille.txt"
-    @read_file_characters = 26
-
-    expected = "Created 'braille.txt' containing 26 characters"
-
-    assert_equal expected, @night_writer.message_to_user
-  end
 end
