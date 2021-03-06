3787eb6478ee65c807e7e88bce25817ce709a9ed
joemecha <jwmecha@gmail.com>
2021-03-05 23:31:32 -0600

    Complete message class tests and methods

--- lib/message.rb
+++ lib/message.rb
@@ -1,2 +1,26 @@
 class Message
+  attr_reader :character_count,
+              :content,
+              :english,
+              :braille
+
+  def initialize
+    @content = ""
+    @character_count = 0
+    @braille = nil
+    @english = nil
+  end
+
+  def add_content(characters)
+    if characters.class == String
+      @content += characters
+      @character_count = @content.length
+      is_braille?
+    end
+  end
+
+  def is_braille?
+    @braille = @content.count("abcdefghijklmnopqrstuvwxyz").zero?
+    @english != @content.count("abcdefghijklmnopqrstuvwxyz").zero?
+  end
 end
--- test/message_test.rb
+++ test/message_test.rb
@@ -5,10 +5,35 @@ require './lib/message'
 class MessageTest < Minitest::Test
   def setup
     @message1 = Message.new
-  end 
+    @message2 = Message.new
+  end
 
   def test_it_exists
     assert_instance_of Message, @message1
   end
 
+  def test_it_has_attributes
+    assert_equal 0, @message1.character_count
+    assert_nil @message1.english
+    assert_nil @message1.braille
+  end
+
+  def test_add_content
+    characters = "some letters"
+    @message1.add_content(characters)
+
+    assert_equal characters, @message1.content
+    assert_equal 12, @message1.character_count
+  end
+
+  def test_is_braille
+    characters = "..0.00.0"
+    @message1.add_content(characters)
+
+    characters2 = "abc."
+    @message2.add_content(characters2)
+
+    assert_equal true, @message1.braille
+    assert_equal false, @message2.braille 
+  end
 end
