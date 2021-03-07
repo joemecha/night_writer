68628d9a667b2a39e1798218d8c89c2b8cac5587
joemecha <jwmecha@gmail.com>
2021-03-07 17:45:55 -0600

    Update dictionary and tests

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -39,12 +39,21 @@ class DictionaryTest < Minitest::Test
   end
 
   def test_braille_english
-    expected = @dictionary.english_braille.invert
-    assert_equal expected, @dictionary.braille_english
+    expected = "d"
+    expected1 = "e"
+    expected2 = "?"
+    assert_equal expected, @dictionary.braille_english[[["00"], [".0"], [".."]]]
+    assert_equal expected1, @dictionary.braille_english[[["0."], [".0"], [".."]]]
+    assert_equal expected2, @dictionary.braille_english[[[".."], ["0."], ["00"]]]
+
   end
 
   def test_braille_numbers
-    expected = @dictionary.numbers_braille.invert 
-    assert_equal expected, @dictionary.braille_numbers
+    expected = "1"
+    expected1 = "3"
+    expected2 = "5"
+    assert_equal expected, @dictionary.braille_numbers[[["0."], [".."], [".."]]]
+    assert_equal expected1, @dictionary.braille_numbers[[["00"], [".."], [".."]]]
+    assert_equal expected2, @dictionary.braille_numbers[[["0."], [".0"], [".."]]]
   end
 end
